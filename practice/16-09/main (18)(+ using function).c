83544d8255b3c4871ee710d0785da59ce6d1ddb3
Rajh1205 <rajhgodhani266@gmail.com>
2025-09-19 09:54:39 +0530

    commit

---
+++ practice/16-09/main (18)(+ using function).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void cal(int a,int b)
+{
+    int c = a+b;
+    printf("%d",c);
+    
+}
+
+int main()
+{
+    cal(8,9);
+    
+    return 0;
+}
---
+++ practice/16-09/main (19).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+//without para without return type
+cal()
+{
+    int a =5;
+    int b =9;
+    
+    int c = a*b;
+    printf("%d",c);
+}
+
+int main()
+{
+    cal();
+    
+    return 0;
+}
