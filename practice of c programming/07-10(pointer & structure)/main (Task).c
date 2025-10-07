cfac7aaa55c0acc16ffd53ebdb83de7315838948
Rajh1205 <rajhgodhani266@gmail.com>
2025-10-07 16:41:09 +0530

    Add files via upload

---
+++ practice of c programming/07-10(pointer & structure)/main (Task).c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int userinput;
+    int luckynum;
+    int maxtries = 5;
+    int tries = 0;
+    
+    luckynum = (rand() % 100);
+    
+    printf("Lucky Number (for testing): %d\n",luckynum);
+    printf("Enter Lucky number between 1 to 100\n");
+    
+    while(tries < maxtries)
+    {
+        printf("Attempt %d: ",tries + 1);
+        scanf("%d",&userinput);
+        tries++;
+        
+        if(userinput == luckynum)
+        {
+            printf(" Congratulations! You guessed the number correctly!\n");
+            
+        }
+        else if(userinput < luckynum)
+        {
+            printf("Try with a higher number.\n ");
+        }
+        else
+        {
+            printf("Try with a lower number.\n");
+        }
+    }
+    
+    printf("Maximum tries (%d) reached. Game Over!\n ",maxtries);
+    printf("The lucky number was: %d\n ",luckynum);
+    
+    return 0;
+    
+}
