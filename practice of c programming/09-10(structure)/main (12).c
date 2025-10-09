d147961e88fa22b78efa7f835aee577f4d26a3b8
Rajh1205 <rajhgodhani266@gmail.com>
2025-10-09 09:50:38 +0530

    Add files via upload

---
+++ practice of c programming/09-10(structure)/main (12).c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+    int id;
+    char name[10];
+    
+}st[50];
+
+void main()
+{
+    int i,num;
+    printf("Enter Number of students: ");
+    scanf("%d",&num);
+    
+    for(i=0;i<num;i++)
+    {
+        printf("Enter id: ");
+        scanf("%d",&st[i].id);
+        
+        printf("Enter name: ");
+        scanf("%s",&st[i].name);
+    }
+    
+    for(i=0;i<num;i++)
+    {
+        printf("\n Your id is %d",st[i].id);
+        printf("\n Your name is %s",st[i].name);
+    }
+    
+    getch();
+}
