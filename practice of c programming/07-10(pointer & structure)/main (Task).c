034e14a879f784c25c301d9f3a6af99aa3d4e157
Rajh1205 <rajhgodhani266@gmail.com>
2025-10-16 16:09:53 +0530

    commit

--- practice of c programming/07-10(pointer & structure)/main (Task).c
+++ practice of c programming/07-10(pointer & structure)/main (Task).c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>  // for rand(), srand()
+#include<time.h>    
 
 int main()
 {
@@ -9,35 +10,37 @@ int main()
     int tries = 0;
 
     srand(time(0));
-    luckynum = (rand() % 100);
-    
-    printf("Lucky Number (for testing): %d\n",luckynum);
-    printf("Enter Lucky number between 1 to 100\n");
-    
-    while(tries < maxtries)
+    luckynum = (rand() % 100) + 1;  
+
+    printf("Lucky Number (for testing): %d\n", luckynum);
+    printf("Enter Lucky Number between 1 to 100\n");
+
+    while (tries < maxtries)
     {
-        printf("Attempt %d: ",tries + 1);
-        scanf("%d",&userinput);
+        printf("Attempt %d: ", tries + 1);
+        scanf("%d", &userinput);
         tries++;
-        
-        if(userinput == luckynum)
+
+        if (userinput == luckynum)
         {
-            printf(" Congratulations! You guessed the number correctly!\n");
-            
+            printf("🎉 Congratulations! You guessed the number correctly!\n");
+            break;
         }
-        else if(userinput < luckynum)
+        else if (userinput < luckynum)
         {
-            printf("Try with a higher number.\n ");
+            printf("Try with a higher number.\n");
         }
         else
         {
             printf("Try with a lower number.\n");
         }
     }
-    
-    printf("Maximum tries (%d) reached. Game Over!\n ",maxtries);
-    printf("The lucky number was: %d\n ",luckynum);
-    
+
+    if (tries == maxtries && userinput != luckynum)
+    {
+        printf("\nMaximum tries (%d) reached. Game Over!\n", maxtries);
+        printf("The lucky number was: %d\n", luckynum);
+    }
+
     return 0;
-    
 }
