77e823343e8b59994d1af5d873eea035dc4309bf
Rajh1205 <rajhgodhani266@gmail.com>
2025-11-19 17:36:53 +0530

    Add files via upload

---
+++ Assignment/ASSIGMENT  PROGRAM/3(Basic Structure).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+ {
+    // Constant declaration
+    const float PI = 3.14;
+
+    
+    int age = 20;            // integer variable
+    char grade = 'A';        // character variable
+    float salary = 45000.50; // float variable
+
+    // Displaying values
+    printf("Constant PI value       : %.2f\n", PI);
+    printf("Integer Age value       : %d\n", age);
+    printf("Character Grade value   : %c\n", grade);
+    printf("Float Salary value      : %.2f\n", salary);
+
+    return 0; // Program ends
+}
